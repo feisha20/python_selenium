5f5ba60c4371ee65cb8757d48697f970f008630e
test <linjt_test@163.com>
2018-07-17 17:35:12 +0800

    newproject

--- myproject/testcase/DEMO1/test_002.py
+++ myproject/testcase/DEMO1/test_002.py
@@ -3,15 +3,27 @@ import sys
 import pytest
 import helper
 
-
+@allure.severity("minor")
+@allure.feature("冒烟测试")
+@allure.story("测试一辆小车")
+@allure.issue('http://www.bug.com/bug004')
+@allure.testcase("如何测试一辆小车")
 def test_car():
     pass
 
-
+@allure.severity("critical")
+@allure.feature("冒烟测试")
+@allure.story("测试一辆自行车")
+@allure.issue('http://www.bug.com/bug005')
+@allure.testcase("如何测试一辆自行车")
 def test_bike():
     pass
 
-
+@allure.severity("blocker")
+@allure.feature("冒烟测试")
+@allure.story("测试一架飞机")
+@allure.issue('http://www.bug.com/bug006')
+@allure.testcase("如何测试一架飞机")
 def test_fly():
     pass
 
