da995daa9d2585f15452d2fbd6cbcd81f07be117
test <linjt_test@163.com>
2018-07-19 08:31:48 +0800

    newproject

--- myproject/testcase/DEMO1/test_001.py
+++ myproject/testcase/DEMO1/test_001.py
@@ -9,8 +9,8 @@ import helper
 @allure.story("test a dog")
 @allure.issue('http://www.bug.com/bug001')
 @allure.testcase("how to test a dog")
-@allure.environment(host="127.0.0.1", report='Allure report', browser='Chrome')
 def test_dog():
+    allure.environment(host="127.0.0.1", report='Allure report', browser='Chrome')
     print("this is a gold dog")
     pass
 
